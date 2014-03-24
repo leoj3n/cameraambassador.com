f5f9331c7d719e4302642af722c505ce13b7fe33
Joel Kuzmarski <leoj3n@gmail.com>
2014-03-24 08:14:51 -0500

    Disable Modernizr and Polymer scripts

--- .openshift/themes/cameraambassadortheme/lib/scripts.php
+++ .openshift/themes/cameraambassadortheme/lib/scripts.php
@@ -29,8 +29,8 @@ function roots_scripts() {
   wp_register_script('polymer', get_template_directory_uri() . '/bower_components/platform/platform.js', array(), null, false);
   wp_register_script('modernizr', get_template_directory_uri() . '/assets/js/vendor/modernizr-2.7.0.min.js', array(), null, false);
   wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', array(), '0b64c0c6f66485bb73b4f27831a51eab', true);
-  wp_enqueue_script('polymer');
-  wp_enqueue_script('modernizr');
+  // wp_enqueue_script('polymer');
+  // wp_enqueue_script('modernizr');
   wp_enqueue_script('jquery');
   wp_enqueue_script('roots_scripts');
 }
